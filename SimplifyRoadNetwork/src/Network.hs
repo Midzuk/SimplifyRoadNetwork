653f694767dc1ab20f081da032c9d3241244be2f
Midzuki <ucdmzk@icloud.com>
2018-10-10 21:54:10 +0900

    修正

--- SimplifyRoadNetwork/src/Network.hs
+++ SimplifyRoadNetwork/src/Network.hs
@@ -26,7 +26,7 @@ data NetworkCsv = NetworkCsv LinkCsv NodeCsv deriving (Show)
 simplifyNetworkCsv :: NetworkCsv -> NetworkCsv
 simplifyNetworkCsv (NetworkCsv lci nci) =
   let 
-    lco = simplifyLinkCsv $ longestLinkCsv lci
+    lco = cutDeadEnd . simplifyLinkCsv $ longestLinkCsv lci
     nodes = f lco :: Set.Set Node
     nco = Map.filterWithKey (\_node _ -> Set.member _node nodes) nci
   in
@@ -59,15 +59,15 @@ simplifyLinkCsv lc =
               let maybeP1 = path <$> maybeLwc1
               let maybeP2 = path <$> maybeLwc2
 
-              lc_ <- get
-              put $ V.cons (LinkWithCond (fromJust $ maybeP1 <> Just p <> maybeP2) cond (fromJust $ maybeS1 <> Just s <> maybeS2)) lc_
+              _lc <- get
+              put $ V.cons (LinkWithCond (fromJust $ maybeP1 <> Just p <> maybeP2) cond (fromJust $ maybeS1 <> Just s <> maybeS2)) _lc
 
         g :: Bool -> State LinkCsv (Maybe LinkWithCond)
         g b =
           state $ \lc ->
-            case V.partition (\_lwc -> h path _lwc) lc of
+            case V.partition (\_lwc -> h (path _lwc)) lc of
               ([lwc1], lc1) ->
-                if cond == linkCond lwc1 && not (any (\_lwc -> h path _lwc) lci)
+                if cond == linkCond lwc1 && not (any (\_lwc -> h (path _lwc)) lci)
                   then (Just lwc1, lc1)
                   else (Nothing, lc)
               _            -> (Nothing, lc)
@@ -90,6 +90,29 @@ cutDeadEnd :: LinkCsv -> LinkCsv
 cutDeadEnd lc = V.filter (\(LinkWithCond p _ _) -> any (\(LinkWithCond _p _ _) -> p `isNextPath` _p) lc && any (\_p -> _p `isNextPath` p) lc) lc
 -}
 
+cutDeadEnd :: LinkCsv -> LinkCsv
+cutDeadEnd lc =
+  (`execState` lc) $ go (path <$> lc)
+  where
+    go :: V.Vector Path -> State LinkCsv ()
+    go ps =
+      do
+        ps1 <- f ps
+        lc1 <- get
+        if V.null ps1
+          then put lc1
+          else go ps1
+
+    f :: V.Vector Path -> State LinkCsv (V.Vector Path)
+    f ps = state $ \ lc ->
+      let
+        dps = V.filter (`deadEnd` lc) ps
+        lc1 = V.filter ((`V.notElem` dps) . path) lc
+      in
+        (dps, lc1)
+
+deadEnd :: Path -> LinkCsv -> Bool
+deadEnd p lc = not (any (\_lwc -> p `isNextPath` path _lwc) lc && any (\_lwc -> path _lwc `isNextPath` p) lc)
 
 {-
 deadEnd :: Path -> LinkCsv -> Bool
